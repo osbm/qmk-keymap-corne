8f070642f929afaa8daa727e1d557f51d283145a
osbm <osmanfbayram@gmail.com>
2025-03-30 03:12:51 +0300

    hotfix

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //,---------------------------------------------------------------.      ,-----------------------------------------------------.
     KC_TAB,        KC_Q,       KC_W,        KC_E,        KC_R,KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
 //|-------+------------+-----------+------------+--------+--------|  |--------+--------+--------+--------+--------+--------|
-   KC_LCTL,LGUI_T(KC_A),LALT_(KC_S),LSFT_T(KC_D),LCTL_T(KC_F),KC_G,   KC_H,RCTL_T(KC_J),RSFT_T(KC_K),LALT_T(KC_L),RGUI_T(KC_SCLN),KC_QUOT,
+   KC_LCTL,LGUI_T(KC_A),LALT_T(KC_S),LSFT_T(KC_D),LCTL_T(KC_F),KC_G,   KC_H,RCTL_T(KC_J),RSFT_T(KC_K),LALT_T(KC_L),RGUI_T(KC_SCLN),KC_QUOT,
 //|-------+------------+-----------+------------+------------+----|  |--------+--------+--------+--------+--------+--------|
    KC_LSFT,        KC_Z,       KC_X,        KC_C,        KC_V,KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
 //|-------+------------+-----------+------------+------------+----|  |--------+--------+--------+--------+--------+--------+--------|
